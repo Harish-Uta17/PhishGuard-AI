4bba6b4428559199a40b40e8bd5dad543eaa802d
Harish-Uta17 <utaharish78@gmail.com>
2026-05-21 18:02:43 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.entity.config_entity import DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -20,10 +21,17 @@ if __name__=='__main__':
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(data_ingestion_artifact=dataingestionartifact, data_validation_config=data_validation_config)
         logging.info("Initiating the data validation")
-        data_validation.initiate_data_validation()
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed and artifact is created")
         print(data_validation_artifact)
+        Data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data transformation started")
+        data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=Data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data transformation completed and artifact is created")
+        
+        
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
